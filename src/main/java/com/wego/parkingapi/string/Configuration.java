12c7b11e4e162c74d112d202d7f70b3e20f92e8e
huynhan47 <huynhan007@gmail.com>
2021-12-17 14:46:26 +0700

    [AnHX] Create configuration set for project

---
+++ src/main/java/com/wego/parkingapi/string/Configuration.java
@@ -0,0 +1,8 @@
+package com.wego.parkingapi.string;
+
+public class Configuration {
+	public static final String carParkAvailabilityUrl ="https://api.data.gov.sg/v1/transport/carpark-availability";
+	public static final String jsonKeyLv1 ="items";
+	public static final String jsonKeyLv2 ="carpark_data";
+	public static final String persistenceUnitName ="parking";	
+}
